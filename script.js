7fed10746662b05c0716672e98037a795d5c33d6
carlo-coding <jaanc.rt@gmail.com>
2022-01-31 13:36:40 -0700

    first commit

--- script.js
+++ script.js
@@ -1,73 +1,81 @@
+const palabraActualContenedor = document.querySelector("#letras");
+// Boton iniciar juego 
+const botonIniciar = document.querySelector("#iniciar");
+const juegoContenedor = document.querySelector(".juego");
+const modal = document.querySelector(".modal");
+const letrasUsadasContenedor = document.querySelector("#letras-usadas");
 
-/* Reglas de encriptación: 
-"e" es convertido para "enter" 
-"i" es convertido para "imes"
-"a" es convertido para "ai"
-"o" es convertido para "ober"
-"u" es convertido para "ufat"
-Solo letras minusculas
-No se permite acentuación de palabras 
-*/
-
-/* Reglas de desencriptación: 
-"enter" es convertido para "e" 
-"imes" es convertido para "i"
-"ai" es convertido para "a"
-"ober" es convertido para "o"
-"ufat" es convertido para "u"
-Solo letras minusculas
-No se permite acentuación de palabras   
-*/
-
-const alfabeto = "abcdefghijklmnñopqrstuvwxyz";
-
-var mensajeAEncriptar = document.querySelector("#input-texto");
-
-var btnEncriptar = document.querySelector("#btn-encriptar");
-
-var btnDesencriptar = document.querySelector("#btn-desencriptar");
-
-var inputResultado = document.querySelector("#msg");
-
-var btnCopiar = document.querySelector("#btn-copy");
-
-
-// toUpperCase()
-
-function encriptar () {
-    var mensaje = mensajeAEncriptar.value.toLowerCase();
-
-    var nuevoMensaje = mensaje.replaceAll("e", "enter")
-                            .replaceAll("i", "imes")
-                            .replaceAll("a", "ai")
-                            .replaceAll("o", "ober")
-                            .replaceAll("u", "ufat");
-
-    inputResultado.value = nuevoMensaje;
+botonIniciar.onclick = function () {
+    juegoContenedor.style.display = "flex";
+    botonIniciar.style.display = "none";
 }
 
-function desencriptar () {
-    var mensaje = mensajeAEncriptar.value;
-    
+var letrasValidas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+shapeStart();
 
-    var nuevoMensaje = mensaje.replaceAll("enter", "e")
-                                    .replaceAll("imes", "i")
-                                    .replaceAll("ai", "a")
-                                    .replaceAll("ober", "o")
-                                    .replaceAll("ufat", "u");
-    inputResultado.value = nuevoMensaje;
+async function obtenerNuevaPalabra() {
+    let palabraValida = false;
+    var palabra = "";
+    while (!palabraValida) {
+        var respuesta = await fetch("https://palabras-aleatorias-public-api.herokuapp.com/random");
+        var datos = await respuesta.json();
+        palabra = datos.body.Word.toUpperCase();
+        for (var letra of palabra) {
+            if (!letrasValidas.includes(letra)){
+                break;
+            }
+            palabraValida = true;
+        }
+    }
+    return palabra;
 }
 
-btnEncriptar.onclick = encriptar;
-
-btnDesencriptar.onclick = desencriptar;
-
-btnCopiar.onclick = function () {
-    navigator.clipboard.writeText(inputResultado.value); // Copiar al portapapeles
-    mensajeAEncriptar.value = "";
-    mensajeAEncriptar.focus();
-    
+function mostrarModal(titulo="", mensaje="", cb=(()=>window.location.assign(""))){
+    modal.querySelector("#titulo").textContent = titulo;
+    modal.querySelector("#mensaje").textContent = mensaje;
+    modal.querySelector(".btn").onclick = cb;
+    modal.style.display = "flex";
 }
 
+async function Juego() {
+    var palabra = await obtenerNuevaPalabra();
+    var letras = palabra.split("");
+    var letrasAdivinadas = [];
+    var letrasUsadas = [];
+    var palabraActual = letras.map(letra => letrasAdivinadas.includes(letra)?` ${letra} `:" _ ").join("");
+    palabraActualContenedor.textContent = palabraActual;
 
-inputResultado.onchange = ()=> console.log("hi")
+    document.addEventListener("keypress", function (e) {
+        let tecla = e.key.toUpperCase();
+        if (letrasUsadas.includes(tecla)){
+            mostrarModal("", `Ya usaste la letra ${tecla}`, ()=>{
+                modal.style.display = "none"
+            })
+            return;
+        }else {
+            letrasUsadas.push(tecla);
+        }
+        letrasUsadasContenedor.textContent = letrasUsadas.join(" ");
+
+        if (letras.includes(tecla)) {
+            letrasAdivinadas.push(tecla);
+            var palabraActual = letras.map(letra => letrasAdivinadas.includes(letra)?` ${letra} `:" _ ").join("");
+            palabraActualContenedor.textContent = palabraActual;
+        }else {
+            if (shapes.length > 1) {
+                shapes.shift()();
+            }else if(shapes.length === 1) {
+                shapes.shift()();
+                mostrarModal("Perdiste", `La palabra era ${palabra}`)
+            }
+            else {
+                mostrarModal("Perdiste", `La palabra era ${palabra}`)
+            }
+        }
+        if(palabraActual?.replaceAll(" ","") === palabra) {
+            mostrarModal("¡Ganaste!", `La palabra es ${palabra}`)
+        }
+    })
+}
+
+Juego();
